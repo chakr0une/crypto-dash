025a2b72b6c3ee44e33b5cd347bed1c7f34be2a6
chakr0une <soufianbenchakroune7@gmail.com>
2025-07-21 21:06:52 +0100

    Add Limit Selector Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import CoinCard from './components/CoinCard';
+import LimitSelector from './components/LimitSelector';
 
 const API_URL = import.meta.env.VITE_COINS_API_URL;
 
@@ -7,16 +8,16 @@ function App() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
         const res = await fetch(
-          `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
         );
         if (!res.ok) throw new Error('Failed to fetch data');
         const data = await res.json();
-        console.log(data);
         setCoins(data);
       } catch (err) {
         setError(err.message);
@@ -26,7 +27,7 @@ function App() {
     };
 
     fetchCoins();
-  }, []);
+  }, [limit]);
 
   return (
     <div>
@@ -37,6 +38,8 @@ function App() {
           <p>❌ {error}</p>
         </div>
       )}
+
+      <LimitSelector limit={limit} onLimitChange={setLimit} />
 
       {!loading && !error && (
         <main className="grid">
